4abd32a78dd6adca7cb6b8e5ac5026aa39e429ea
bryan <80012381+Youheyo@users.noreply.github.com>
2022-10-03 10:46:04 +0800

    Added partially finished cube spawn script

--- Assets/Scripts/EventExercise/CubeSpawnScript.cs
+++ Assets/Scripts/EventExercise/CubeSpawnScript.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class CubeSpawnScript : MonoBehaviour
 {
+
+    [SerializeField] GameObject SpawnPointCube;
     // Start is called before the first frame update
     void Awake()
     {
-        EventBroadcaster.Instance.AddObserver(EventNames.SPAWN_OBJECT, )
+        EventBroadcaster.Instance.AddObserver(EventNames.Event_Exercise.CUBE_SPAWN, SpawnCube);
     }
 
     // Update is called once per frame
 
     void OnDestroy()
     {
-        EventBroadcaster.Instance.RemoveObserver(EventNames.SPAWN_OBJECT);
+        EventBroadcaster.Instance.RemoveObserver(EventNames.Event_Exercise.CUBE_SPAWN);
     }
 
     private void SpawnCube(Parameters param)
     {
+        int cubeSpawnAmt = param.GetIntExtra("cube", 1);
 
+        for (int i = 0; i < cubeSpawnAmt; i++)
+        {
+            GameObject obj = Instantiate(SpawnPointCube, this.SpawnPointCube.transform);
+        }
     }
 }
