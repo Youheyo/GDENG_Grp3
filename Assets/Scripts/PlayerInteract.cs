be77f183a67ec94a1d2214d0bf312e6f5e56d5f2
Youheyo <80012381+Youheyo@users.noreply.github.com>
2022-10-15 23:45:35 +0800

    Made the camera raycast and get object 2 units forward. It displays name of the raycasted obj on console.
    Interact is set on LMB
    Added a temporary crosshair
    Added "interactable" tag and layer

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInteract : MonoBehaviour
+{
+	private GameObject raycastedObj;
+	[SerializeField] private int rayLength = 10;
+	[SerializeField] private LayerMask layerMaskInteract;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+		if(Physics.Raycast(transform.position, fwd, out hit, rayLength, layerMaskInteract))
+		{
+			if (hit.collider.CompareTag("interactable"))
+			{
+				raycastedObj = hit.collider.gameObject;
+
+				if (Input.GetKeyDown(KeyCode.Mouse0))
+				{
+					Debug.Log("Interacted with " + raycastedObj.name);
+					raycastedObj.SetActive(false);
+				}
+			}
+		}
+    }
+}
